91535b0f43598da67ad2047e3e68a7925a168a19
Jordan <jallibad@gmail.com>
2017-01-25 09:00:42 -0500

    Lab 1

--- lab1/fib2.py
+++ lab1/fib2.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 def fib2(n):
     if n==0:
         return 0
-    f = [1]*n
+    f = [1]*(n+1)
+    f[0] = 0
     for i in range(2,n+1):
-        f.append(f[i-1] + f[i-2])
+        f[i] = f[i-1] + f[i-2]
     return f[n]
 
-ns = [2**10,2**12,2**13,2**16,2**18,2**19]
+#ns = [2**10,2**12,2**13,2**16,2**18,2**19]
+ns = [1,5,10,15,20,25,30,35,40,41,42,43]
 results = []
 for n in ns:
     start_time = time.time()
