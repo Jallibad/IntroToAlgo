de05644b05138d6bd2e6dc87f0c8aca9e4fb804f
Jordan <jallibad@gmail.com>
2017-03-08 09:21:54 -0500

    Lab 5

--- lab5/penis.py
+++ lab5/penis.py
@@ -2,10 +2,7 @@ def reverseGraph(g):
     ans = {}
     for a, b in g.items():
         for i in b:
-            if i in ans:
-                ans[i].append(a)
-            else:
-                ans[i] = [a]
+            ans.setdefault(i, []).append(a)
     return ans
 
 def buildGraph(file):
@@ -18,15 +15,39 @@ def buildGraph(file):
             graph[curLine[0]].append(curLine[1])
     return graph
 
+def explore(a, graph, visited, post, p=False):
+    visited[a] = True
+    for u in graph.get(a, []):
+        if not visited.get(u):
+            if p:
+                print(u, end=' ')
+            explore(u, graph, visited, post, p)
+    post.insert(0, a)
+
 def main():
     #sys.setrecursionlimit(1000) #increase recursion limit from 400 to 1000
     
     graph = buildGraph("example.txt")
     reverse = reverseGraph(graph)
-    print(graph)
+    #print(graph)
+    #print(reverse)
     
-    #test 2: operate on test.txt
-    
-    
+    visited = {}
+    for a in graph:
+        visited[a] = False
+
+    post = []
+    for a in reverse:
+        if not visited.get(a):
+            explore(a, reverse, visited, post)
+    #print(post)
+
+    visited = {}
+    for u in post:
+        if not visited.get(u):
+            print(u, end=' ')
+            explore(u, graph, visited, [], True)
+            print()
+
 if __name__ == "__main__":
     main()
