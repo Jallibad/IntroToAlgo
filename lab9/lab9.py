b1aa93c1d36674d7d50269fc6f30b4d10de0f316
Jordan <jallibad@gmail.com>
2017-04-26 02:50:44 -0400

    Lab9

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -14,14 +14,7 @@ class Graph(object):
         self.edges.setdefault(from_node,[]).append(to_node)
         self.edges.setdefault(to_node,[]).append(from_node)
         self.distances[(from_node, to_node)] = distance
-
-g = Graph()
-with open("example.txt") as file:
-    for line in file:
-        s,t,v = line.split(" ")
-        g.add_node(s)
-        g.add_node(t)
-        g.add_edge(s,t,int(v))
+        self.distances[(to_node, from_node)] = 0
 
 def dijkstra(graph, initial):
     visited = {initial: 0}
@@ -41,12 +34,14 @@ def dijkstra(graph, initial):
         nodes.remove(u)
         current_weight = visited[u]
 
+        if u not in graph.edges:
+            continue
         for edge in graph.edges[u]:
             try:
                 weight = current_weight + graph.distances[(u, edge)]
             except:
                 continue
-            if edge not in visited or weight < visited[edge]:
+            if (edge not in visited or weight < visited[edge]) and graph.distances[(u,edge)]>0:
                 visited[edge] = weight
                 path[edge] = u
 
@@ -70,7 +65,35 @@ def shortest_path(graph, origin, destination):
 
     return visited[destination], list(full_path)
 
+def explore(s, g, visited=set()):
+    visited.add(s)
+    for u in g.edges[s]:
+        if u not in visited and g.distances[(s,u)]>0:
+            explore(u, g, visited)
+    return visited
+
 def fordFulkerson(g, source, sink):
     max_flow = 0
-    path = dijkstra(
-    while 
+    a, path = shortest_path(g, source, sink)
+    while a != 0:
+        flow = g.distances[(path[0],path[1])]
+        for i in range(1,len(path)):
+            flow = min(flow, g.distances[(path[i-1],path[i])])
+        max_flow += flow
+        for i in range(1,len(path)):
+            g.distances[(path[i-1],path[i])] -= flow
+            g.distances[(path[i],path[i-1])] += flow
+            
+        a, path = shortest_path(g, source, sink)
+    visited = explore(source,g)
+    print("max flow =", max_flow)
+    print("minimum cut =",max_flow, sorted(visited), sorted(g.nodes - visited))
+
+g = Graph()
+with open("example.txt") as file:
+    for line in file:
+        s,t,v = line.split(" ")
+        g.add_node(int(s))
+        g.add_node(int(t))
+        g.add_edge(int(s),int(t),int(v))
+fordFulkerson(g,min(g.nodes),max(g.nodes))
